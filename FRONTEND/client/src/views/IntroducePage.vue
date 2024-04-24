f30d8aac64a471e1c072118a98d82eb732d91002
B2014590_Trương Trí Ngoan <ngoanb2014590@student.ctu.edu.vn>
2024-04-24 21:18:31 +0700

    update

---
+++ FRONTEND/client/src/views/IntroducePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="page">
+    <h1 class="bd-title d-flex justify-content-center mt-4" style="font-weight: 600">
+      GIỚI THIỆU
+    </h1>
+    <div class="container my-5">
+      <div class="row featurette d-flex justify-content-center align-items-center">
+        <div class="col-md-7">
+          <p class="lead">
+            Một thư viện là một kho sưu tập các nguồn thông tin, được chọn lựa bởi các chuyên gia và có thể được tiếp
+            cận để tham khảo hay mượn, thường là trong một môi trường yên tĩnh phù hợp cho học tập. Kho tàng của một thư
+            viên có thể chứa đến hàng triệu đầu mục, bao gồm nhiều định dạng như sách, ấn phẩm định kỳ, báo, thủ bản,
+            phim, bản đồ, bản in, văn kiện, CD, cassette, băng video, DVD, đĩa Blu-ray, sách điện tử, sách nói, cơ sỏ dữ
+            liệu, video game và các thể loại khác.
+            <br><br>
+
+            Một thư viện được xây dựng và bảo quản bởi một cơ quan nhà nước, một tổ chức, một công ty, hoặc một cá nhân.
+            Ngoài việc cung cấp tài liệu, thư viện còn được phục vụ bởi các thủ thư, những chuyên gia trong việc tìm
+            kiếm và sắp xếp thông tin và đáp ứng nhu cầu của người dùng. Thư viện cũng thường có khu vực yên tĩnh để học
+            tập, và những khu vực hỗ trợ học và làm việc nhóm. Nhiều thư viện có cơ sở thiết bị có thể truy cập kho tài
+            liệu số và mạng Internet.
+            <br><br>
+
+            Thư viện hiện đại đang ngày càng được hướng đến trở thành nơi tiếp cận thông tin và kiến thức không giới hạn
+            qua nhiều hình thức và nguồn khác nhau. Thư viện ngày càng trở thành những trung tâm cộng đồng nơi thực hiện
+            các chương trình công cộng và hỗ trợ mọi người có thể học tập suốt đời.
+          </p>
+        </div>
+        <div class="col-md-5">
+          <img
+            src="https://th.bing.com/th/id/OIP.rSAzW-6MnDYh9rLekoJMoQHaDt?w=277&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+            class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500"
+            alt="" />
+        </div>
+      </div>
+
+      <hr class="featurette-divider" />
+    </div>
+  </div>
+</template>
